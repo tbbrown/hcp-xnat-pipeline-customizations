de0d002d5cd4bc52cfe6ec2adc0568cd90bef011
Michael Hileman <mhilema@gmail.com>
2014-07-21 16:18:21 -0500

    Some MEG project individual subject structural preprocessing scripts

---
+++ pipeline/catalog/ToolsHCP/resources/scripts/MegLaunches/SubmitStructural.Subject.715950.sh
@@ -0,0 +1,26 @@
+printf "Connectome DB Username: "
+read userid
+
+stty -echo
+printf "Connectome DB Password: "
+read password
+echo ""
+stty echo
+
+subj="715950"
+
+python ../launchHCP.py \
+-User ${userid} \
+-Password ${password} \
+-Server db.humanconnectome.org \
+-Project HCP_Staging \
+-Subjects ${subj} \
+-LaunchDiffusion 0 \
+-LaunchFunctional 0 \
+-LaunchStructural 1 \
+-LaunchMPP 1 \
+-Launch 1 \
+-Compute CHPC \
+-LaunchTaskAnalysis 0 \
+-LaunchICAAnalysis 0 \
+-Shadow 1,2,3,4,5,6,7,8
