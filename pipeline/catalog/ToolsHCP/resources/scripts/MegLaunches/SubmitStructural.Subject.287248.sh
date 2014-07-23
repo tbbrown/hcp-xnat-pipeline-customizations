2184e3ff9cf441fa39ceeef4758f9c301fb614a3
Michael Hileman <mhilema@gmail.com>
2014-07-23 15:37:20 -0500

    Added a couple subject specific meg scripts

---
+++ pipeline/catalog/ToolsHCP/resources/scripts/MegLaunches/SubmitStructural.Subject.287248.sh
@@ -0,0 +1,27 @@
+printf "Connectome DB Username: "
+read userid
+
+stty -echo
+printf "Connectome DB Password: "
+read password
+echo ""
+stty echo
+
+subj="287248"
+
+python ../launchHCP.py \
+-Debug \
+-User ${userid} \
+-Password ${password} \
+-Server db.humanconnectome.org \
+-Project HCP_Staging \
+-Subjects ${subj} \
+-LaunchDiffusion 0 \
+-LaunchFunctional 0 \
+-LaunchStructural 1 \
+-LaunchMPP 1 \
+-Launch 1 \
+-Compute CHPC \
+-LaunchTaskAnalysis 0 \
+-LaunchICAAnalysis 0 \
+-Shadow 1,2,3,4,5,6,7,8
